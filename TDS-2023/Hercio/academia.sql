c0ae4d598237abf760ff4055ff42be8a95a51619
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-05-28 00:41:32 +0000

    terminemo a academia

--- TDS-2023/Hercio/academia.sql
+++ TDS-2023/Hercio/academia.sql
@@ -18,5 +18,26 @@ create table Reservas (ID_Reserva int AUTO_INCREMENT PRIMARY KEY,
                        ID_Membro_Reservado int,
                        FOREIGN KEY (ID_Membro_Reservado) REFERENCES chad(ID_Chad),
                        ID_Treinador int,
-                       FOREIGN KEY (ID_Treinador) REFERENCES treinador(ID_Treinador));
+                       FOREIGN KEY (ID_Treinador) REFERENCES treinador(ID_Treinador),
+                       ID_Equipamento int,
+                       FOREIGN KEY (ID_Equipamento) REFERENCES equipamento(ID_Equipamento));
 
+create table Equipamento (ID_Equipamento int AUTO_INCREMENT PRIMARY KEY,
+                       	ID_Treinador int,
+                       	FOREIGN KEY (ID_Treinador) REFERENCES treinador(ID_Treinador),
+                        nome varchar(255),
+                        utilidade varchar(255));
+
+create table pagamento (ID_Pagamento int AUTO_INCREMENT PRIMARY KEY,
+                        ID_Chad int,
+                        FOREIGN KEY (ID_Chad) REFERENCES chad(ID_Chad),
+                       mes int,
+                       valor double);
+
+create table Aula (ID_Aula int AUTO_INCREMENT PRIMARY KEY,
+                  ID_Chad int,
+                   FOREIGN KEY (ID_Chad) REFERENCES chad(ID_Chad),
+                  ID_Treinador int,
+                   FOREIGN KEY (ID_Treinador) REFERENCES treinador(ID_Treinador),
+                  ID_Equipamento int,
+                  FOREIGN KEY (ID_Equipamento) REFERENCES equipamento(ID_Equipamento));
