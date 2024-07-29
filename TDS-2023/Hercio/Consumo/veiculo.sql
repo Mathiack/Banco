16ea5c47c723ad43561fa623d9f3689c10cdc5a3
Thunder <151688223+Mathiack@users.noreply.github.com>
2024-07-29 23:46:05 +0000

    ok

--- TDS-2023/Hercio/Consumo/veiculo.sql
+++
@@ -1,5 +0,0 @@
-create table veiculo (ID_Veiculo int AUTO_INCREMENT PRIMARY KEY,
-                      marca varchar(255),
-                      modelo varchar(255),
-                      kmInicial double,
-                      kmFinal double);
